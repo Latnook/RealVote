ac5a6f9f215acf82df1756d0e8e3064504b95719
Ariel Palatnik <8847444@proton.me>
2026-08-08 20:40:07 +0300

    feat(tf): lambda, iam role, log group

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.60"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
   }
   backend "s3" {}   # configured via -backend-config=backend.hcl
 }
